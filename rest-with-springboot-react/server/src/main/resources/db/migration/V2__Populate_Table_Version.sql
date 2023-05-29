f9599c65391a1d5fe288c47765f4ea60705cb6bb
Yuri Ramos Costa <102838410+YuriRCosta@users.noreply.github.com>
2023-05-29 01:54:35 -0300

    Delete rest-with-springboot-react/server/src directory

--- rest-with-springboot-react/server/src/main/resources/db/migration/V2__Populate_Table_Version.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
-    (1, 'Sao Paulo', 'Yuri', 'Male', 'Costa'),
-    (3, 'Capao da Canoa', 'Yasmin', 'Female', 'Ferreira Dadda'),
-    (6, 'Capao da Canoa', 'Yasminasd', 'Female', 'Ferreira Daddaasdas'),
-    (7, 'Capao da Canoa', 'Yasminasd', 'Female', 'Ferreira Daddaasdas');
-
--- rest-with-springboot-react/server/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
+++
@@ -1,3 +0,0 @@
-﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('leandro', 'Leandro Costa', 'f68897e3c36fe14324decaa3fa9cd5cc14fd55a2026adc7315ae33dadfae5967f0a0de3e9dbe3833', b'1', b'1', b'1', b'1'),
-	('flavio', 'Flavio Costa', 'c26935bf823d0a32e619019e083135210525e6c86bd309e715f541a12ab4c8e8f16a3b3770043c25', b'1', b'1', b'1', b'1');
