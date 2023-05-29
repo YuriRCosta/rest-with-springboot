fbb3a18b5da28adf8d53df9ea0bdc4a426b1c502
Yuri Ramos Costa <102838410+YuriRCosta@users.noreply.github.com>
2023-05-29 01:55:15 -0300

    Delete src directory

--- src/main/java/br/com/yuri/restwithspringboot/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.yuri.restwithspringboot.repositories;
-
-import br.com.yuri.restwithspringboot.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
